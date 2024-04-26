05f777cfb3782a4e4b8a6a5f05e315aacd373ed8
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-04-27 00:19:14 +0300

    test

--- app/main.py
+++ app/main.py
@@ -11,11 +11,9 @@ SHEET_ID = '1xkhXmZlNsyCYC4qiMrwywcVJl-EhZ_2IqD5xOvMzVmw'
 def main():
     sheets_service = GoogleSheetsService(SHEET_ID, date.today().strftime('%d.%m.%Y'))
 
-    # all_urls = sheets_service.get_all_urls()
+    all_urls = sheets_service.get_all_urls()
     parser = TelderiParser(sheets_service)
-    # parser.parse_sites_data(predicate=lambda url: url not in all_urls)
-    parser.parse_sites_data()
-    # sheets_service.add_telderi_rows(telderi_data)
+    parser.parse_sites_data(predicate=lambda url: url not in all_urls)
     print('Success!')
 
 
