dd8eb564f992c8a2da5113e47db69a2141d79bda
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-04-28 00:05:15 +0300

    test-urls-from-file

--- app/main.py
+++ app/main.py
@@ -11,8 +11,8 @@ SHEET_ID = '1xkhXmZlNsyCYC4qiMrwywcVJl-EhZ_2IqD5xOvMzVmw'
 def main():
     sheets_service = GoogleSheetsService(SHEET_ID, date.today().strftime('%d.%m.%Y'))
 
-    # all_urls = sheets_service.get_all_urls()
-    all_urls = set(map(lambda line: line.strip(), open('urls.txt').readlines()))
+    all_urls = sheets_service.get_all_urls()
+    # all_urls = set(map(lambda line: line.strip(), open('urls.txt').readlines()))
 
     def predicate(url):
         r = url not in all_urls
